dcf9237db883788044ec2d1c06cbf7dd41f00ba2
Sayeed Ajmal <sayeedajmala06@gmail.com>
2022-07-06 15:32:04 +0530

    Playing Audio with Correct File Fixed

--- app/src/main/java/com/Strong/quranfy/MediaHandler.java
+++ app/src/main/java/com/Strong/quranfy/MediaHandler.java
@@ -1,13 +1,39 @@
 package com.Strong.quranfy;
 
-import com.Strong.quranfy.Models.AudioUri;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
 
 public class MediaHandler {
+    static MediaPlayer mediaPlayer;
 
-    //Method of PlayAudio
-    public void playAudio() {
-        AudioUri audioUri = new AudioUri();
-        String audiURL = audioUri.getURL();
-        System.out.println("<<<<<<<<<<" + audiURL);
+    public void MediaPlay(String uri){
+        mediaPlayer=new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(uri);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            String duration=createDuration(mediaPlayer.getDuration());
+            System.out.println("<<<<<<<<"+duration);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String createDuration(int duration) {
+
+        String time = "";
+        int min = duration / 1000 / 60;
+        int sec = duration / 1000 % 60;
+
+        time = time + min + ":";
+
+        if (sec < 10) {
+
+            time += "0";
+
+        }
+        time += sec;
+        return time;
+
     }
 }
