e3acd305f11d66495249b7d0a21a0bc87e1bf8f0
Sayeed Ajmal <sayeedajmala06@gmail.com>
2022-08-03 08:41:54 +0530

    changing the Setting of Player in playScreen.java

--- app/src/main/java/com/Strong/quranfy/playScreen.java
+++ app/src/main/java/com/Strong/quranfy/playScreen.java
@@ -2,13 +2,17 @@ package com.Strong.quranfy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.Strong.quranfy.Models.surahData;
 import com.Strong.quranfy.databinding.ActivityPlayScreenBinding;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,7 +20,10 @@ import java.util.TimerTask;
 public class playScreen extends AppCompatActivity {
     ActivityPlayScreenBinding BindPlayScreen;
     int favouriteFlag = 0;
-    MediaHandler mediaHandler = new MediaHandler();
+    String currentTime;
+    String TotalTime;
+    static int duration, currentDuration;
+    static MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,34 +51,117 @@ public class playScreen extends AppCompatActivity {
         BindPlayScreen.backbutton.setOnClickListener(view -> onBackPressed());
 
         BindPlayScreen.PlayPauseButton.setOnClickListener(view -> {
-            mediaHandler.PlayPause(BindPlayScreen.PlayPauseButton, getApplicationContext());
+            PlayPause(getApplicationContext(), BindPlayScreen.PlayPauseButton);
         });
 
         //Setting Current Duration
         Handler current = new Handler();
-        final int delay = 100;
+        final int delay = 1000;
         current.postDelayed(new Runnable() {
             @Override
             public void run() {
                 //Setting the current duration from the media player
-                String currentTime = mediaHandler.createDuration(MediaHandler.getCurrentDuration());
+                currentTime = createDuration(getCurrentDuration());
                 BindPlayScreen.StartTime.setText(currentTime);
 
                 //Setting TotalTime of Surah
-                String TotalTime = mediaHandler.createDuration(MediaHandler.getDuration());
+                TotalTime = createDuration(getDuration());
                 BindPlayScreen.StopTime.setText(TotalTime);
                 current.postDelayed(this, delay);
+
+                BindPlayScreen.seekBar.setMax(getDuration());
+                BindPlayScreen.seekBar.setProgress(getCurrentDuration());
+
             }
         }, delay);
+    }
 
-        //Max Seekbar
-        Toast.makeText(this, "Total " + MediaHandler.getDuration(), Toast.LENGTH_SHORT).show();
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                Toast.makeText(playScreen.this, " Current " +
-                        MediaHandler.getCurrentDuration(), Toast.LENGTH_SHORT).show();
+    public static void MediaPlay(String uri) {
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(uri);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+            //Setting Total Duration of MediaPlayer
+            setDuration(mediaPlayer.getDuration());
+
+            //Setting Current Duration of MediaPlayer
+            Handler handler = new Handler();
+            final int delay = 1000;
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //Getting the current duration from the media player
+                    setCurrentDuration(mediaPlayer.getCurrentPosition());
+
+                    handler.postDelayed(this, delay);
+                }
+            }, delay);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String createDuration(int duration) {
+
+        String time = "";
+        int min = duration / 1000 / 60;
+        int sec = duration / 1000 % 60;
+
+        time = time + min + ":";
+
+        if (sec < 10) {
+
+            time += "0";
+
+        }
+        time += sec;
+        return time;
+
+    }
+
+    public static void PlayPause(Context context, ImageButton imageButton) {
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                //imageButton.setImageResource(R.drawable.play);
+                mediaPlayer.pause();
+            } else {
+                // imageButton.setImageResource(R.drawable.pause);
+                mediaPlayer.start();
             }
-        }, 0, 900);
+        } else {
+            Toast.makeText(context, "First Select A Surah", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static int getDuration() {
+        return duration;
+    }
+
+    public static void setDuration(int duration) {
+        playScreen.duration = duration;
+    }
+
+    public static int getCurrentDuration() {
+        return currentDuration;
+    }
+
+    public static void setCurrentDuration(int currentDuration) {
+        playScreen.currentDuration = currentDuration;
+    }
+
+    public void Destroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+    }
+
+    public static void CheckMediaPlaying() {
+        if (mediaPlayer != null) {
+            //we will start mediaPlayer if currently there is no songs in it
+            mediaPlayer.start();
+            mediaPlayer.release();
+        }
     }
 }
