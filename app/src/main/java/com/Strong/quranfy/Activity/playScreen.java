ffedea78c6de1bafd721587e8cfe14b5d4cf9261
SAYEED AJMAL <sayeedajmala06@gmail.com>
2023-02-11 15:09:08 +0530

    changed the Color Compat Theme and Added Lyrics Options

--- app/src/main/java/com/Strong/quranfy/Activity/playScreen.java
+++ app/src/main/java/com/Strong/quranfy/Activity/playScreen.java
@@ -8,53 +8,30 @@ import static com.Strong.quranfy.Activity.mediaService.setFlag;
 import static com.Strong.quranfy.R.drawable.pause;
 import static com.Strong.quranfy.R.drawable.play;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.Strong.quranfy.Models.surahData;
-import com.Strong.quranfy.R;
 import com.Strong.quranfy.databinding.ActivityPlayScreenBinding;
+import com.dirror.lyricviewx.LyricEntry;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.io.File;
+import java.util.ArrayList;
 
 
 public class playScreen extends AppCompatActivity {
-    static ActivityPlayScreenBinding BindPlayScreen;
-    int favouriteFlag = 0;
     public static String currentSurahNumber;
+    static ActivityPlayScreenBinding Bind;
     static String currentTime;
+    int favouriteFlag = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        BindPlayScreen = ActivityPlayScreenBinding.inflate(getLayoutInflater());
-        setContentView(BindPlayScreen.getRoot());
-
-        //Setting Data Of List to PlayScreen by ItemViewClick
-        BindPlayScreen.surahName.setText(surahData.getSurahName());
-       /* BindPlayScreen.SurahNumber.setText(surahData.getSurahNumber());
-        BindPlayScreen.ParticularSurahName.setText(surahData.getSurahName());
-        BindPlayScreen.SurahInformation.setText(surahData.getSurahInform());*/
-
-        //setting Favourite
-        BindPlayScreen.backPlayScreen.setOnClickListener(view -> onBackPressed());
-
-        favourite();
-        PlayPause();
-        seekBar();
-        NextTrack();
-        PrevTrack();
-    }
-
-    private static void playLyrics(long progress) {
-        File file = new File(String.valueOf(R.raw._01));
-        BindPlayScreen.lyrics.setLyricFile(file);
-        BindPlayScreen.lyrics.setOnPlayerClickListener((progress1, content) -> BindPlayScreen.lyrics.setCurrentTimeMillis(progress1));
-    }
 
     public static void currentDuration() {
         Handler current = new Handler();
@@ -64,22 +41,18 @@ public class playScreen extends AppCompatActivity {
             public void run() {
                 //Setting TotalTime of Surah
                 String TotalDuration = createDuration(getDuration());
-                BindPlayScreen.TotalTime.setText(TotalDuration);
+                Bind.TotalTime.setText(TotalDuration);
                 try {
                     //Setting the current duration from the media player
-                    BindPlayScreen.progressBar.setMax(mediaPlayer.getDuration());
+                    Bind.progressBar.setMax(mediaPlayer.getDuration());
                     currentTime = createDuration(mediaPlayer.getCurrentPosition());
-                 /*   BindPlayScreen.currentTime.setText(currentTime);
-                    BindPlayScreen.lyrics.setCurrentTimeMillis(mediaPlayer.getCurrentPosition());*/
-/*
-                    BindPlayScreen.lyrics.setLyricFile(new File(String.valueOf(R.raw._01)));
-*/
+                    Bind.currentTime.setText(currentTime);
                     if (currentTime.equals(TotalDuration)) {
-                        BindPlayScreen.PlayPauseButton.setImageResource(play);
+                        Bind.PlayPauseButton.setImageResource(play);
                         setFlag(0);
                     }
                     //Setting progressBar of Slider
-                    BindPlayScreen.progressBar.setProgress(mediaPlayer.getCurrentPosition(), true);
+                    Bind.progressBar.setProgress(mediaPlayer.getCurrentPosition(), true);
                 } catch (IllegalStateException e) {
                     e.printStackTrace();
                 }
@@ -88,24 +61,65 @@ public class playScreen extends AppCompatActivity {
         }, delay);
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bind = ActivityPlayScreenBinding.inflate(getLayoutInflater());
+        setContentView(Bind.getRoot());
+
+        Bind.surahName.setText(surahData.getSurahName());
+
+        PlayPause();
+        seekBar();
+        NextTrack();
+        PrevTrack();
+        pdf();
+    }
+
+    private void pdf() {
+        // Bind.lyrics.fromStream(getResources().openRawResource(R.raw._02)).enableSwipe(true).load();
+        ArrayList<LyricEntry> lyricEntries = new ArrayList<>();
+        lyricEntries.add(new LyricEntry(0, "بِسْمِ اللهِ الرَّحْمَٰنِ الرَّحِيمِ"));
+        lyricEntries.add(new LyricEntry(1, "الْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِين"));
+        lyricEntries.add(new LyricEntry(2, "الرَّحْمٰنِ الرَّحِيمِ"));
+        lyricEntries.add(new LyricEntry(3, "مٰلِكِ يَوْمِ الدِّينِِ"));
+        lyricEntries.add(new LyricEntry(4, "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُِِ"));
+        lyricEntries.add(new LyricEntry(5, "اهْدِنَا الصِّرٰطَ الْمُسْتَقِيمَ"));
+        lyricEntries.add(new LyricEntry(6, "صِرٰطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّآلِّين"));
+        Bind.lyrics.loadLyric(lyricEntries);
+        Bind.lyrics.setCurrentTextSize(100);
+        Bind.lyrics.setNormalTextSize(80);
+        Bind.lyrics.setTextGravity(Gravity.END);
+        Bind.lyrics.setTimelineTextColor(Color.rgb(9, 162, 189));
+        Bind.lyrics.setCurrentColor(Color.rgb(9, 162, 189));
+        Bind.lyrics.setDraggable(true, l -> {
+            Bind.lyrics.updateTime(l, true);
+            Toast.makeText(playScreen.this, "You Clicked on PlayButton", Toast.LENGTH_SHORT).show();
+            return false;
+        });
+
+        Bind.lyrics.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+        Bind.lyrics.setTimelineColor(Color.parseColor("green"));
+    }
+
     private void NextTrack() {
-        BindPlayScreen.NextTrackButton.setOnClickListener(view -> {
+        Bind.NextTrackButton.setOnClickListener(view -> {
            /* int surahNumber = Integer.parseInt(currentSurahNumber) + 1;
             surah_adaptor.getAudioFile(String.valueOf(surahNumber));*/
-            Snackbar.make(BindPlayScreen.NextTrackButton, "Feature is on Way. So go Back and Select Surah", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(Bind.NextTrackButton, "Feature is on Way. So go Back and Select Surah", Snackbar.LENGTH_SHORT).show();
         });
     }
 
     private void PrevTrack() {
-        BindPlayScreen.NextTrackButton.setOnClickListener(view -> {
+        Bind.PreviousTrackButton.setOnClickListener(view -> {
            /* int surahNumber = Integer.parseInt(currentSurahNumber) - 1;
             surah_adaptor.getAudioFile(String.valueOf(surahNumber));*/
-            Snackbar.make(BindPlayScreen.NextTrackButton, "Feature is on Way. So go Back and Select Surah", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(Bind.NextTrackButton, "Feature is on Way. So go Back and Select Surah", Snackbar.LENGTH_SHORT).show();
         });
     }
 
     private void seekBar() {
-        BindPlayScreen.progressBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        Bind.progressBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
@@ -124,35 +138,24 @@ public class playScreen extends AppCompatActivity {
     }
 
     private void PlayPause() {
-        BindPlayScreen.PlayPauseButton.setOnClickListener(view -> {
+        Bind.PlayPauseButton.setOnClickListener(view -> {
             setFlag(mediaService.PlayPause());
             if (flag == 0 | flag == 2) {
-                BindPlayScreen.PlayPauseButton.setImageResource(play);
+                Bind.PlayPauseButton.setImageResource(play);
             } else {
-                BindPlayScreen.PlayPauseButton.setImageResource(pause);
+                Bind.PlayPauseButton.setImageResource(pause);
             }
         });
     }
 
-    private void favourite() {
-        BindPlayScreen.selectFavourite.setOnClickListener(view -> {
-            if (favouriteFlag == 0) {
-                BindPlayScreen.selectFavourite.setImageResource(R.drawable.favouritefilled);
-                favouriteFlag = 1;
-            } else {
-                BindPlayScreen.selectFavourite.setImageResource(R.drawable.favouriteunfilled);
-                favouriteFlag = 0;
-            }
-        });
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (flag == 0 | flag == 2) {
-            BindPlayScreen.PlayPauseButton.setImageResource(play);
+            Bind.PlayPauseButton.setImageResource(play);
         } else {
-            BindPlayScreen.PlayPauseButton.setImageResource(pause);
+            Bind.PlayPauseButton.setImageResource(pause);
         }
     }
 }
